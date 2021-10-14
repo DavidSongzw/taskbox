c8e551e45c8316b5ac9415e3c55ace7828e27a42
宋振威 <zhenwei.song@kuwo.cn>
2021-10-14 16:45:35 +0800

    change task background to red

--- src/components/Task.js
+++ src/components/Task.js
@@ -1,6 +1,10 @@
-import React from 'react';
-import PropTypes from 'prop-types';
-export default function Task({ task: { id, title, state }, onArchiveTask, onPinTask }) {
+import React from 'react'
+import PropTypes from 'prop-types'
+export default function Task({
+  task: { id, title, state },
+  onArchiveTask,
+  onPinTask,
+}) {
   return (
     <div className={`list-item ${state}`}>
       <label className="checkbox">
@@ -9,15 +13,20 @@ export default function Task({ task: { id, title, state }, onArchiveTask, onPinT
           defaultChecked={state === 'TASK_ARCHIVED'}
           disabled={true}
           name="checked"
-          style={{ background: 'red' }}
         />
         <span className="checkbox-custom" onClick={() => onArchiveTask(id)} />
       </label>
       <div className="title">
-        <input type="text" value={title} readOnly={true} placeholder="Input title" />
+        <input
+          type="text"
+          value={title}
+          readOnly={true}
+          placeholder="Input title"
+          style={{ background: 'red' }}
+        />
       </div>
 
-      <div className="actions" onClick={event => event.stopPropagation()}>
+      <div className="actions" onClick={(event) => event.stopPropagation()}>
         {state !== 'TASK_ARCHIVED' && (
           // eslint-disable-next-line jsx-a11y/anchor-is-valid
           <a onClick={() => onPinTask(id)}>
@@ -26,21 +35,21 @@ export default function Task({ task: { id, title, state }, onArchiveTask, onPinT
         )}
       </div>
     </div>
-  );
+  )
 }
 
 Task.propTypes = {
- /** Composition of the task */
- task: PropTypes.shape({
-   /** Id of the task */
-   id: PropTypes.string.isRequired,
-   /** Title of the task */
-   title: PropTypes.string.isRequired,
-   /** Current state of the task */
-   state: PropTypes.string.isRequired,
- }),
- /** Event to change the task to archived */
- onArchiveTask: PropTypes.func,
- /** Event to change the task to pinned */
- onPinTask: PropTypes.func,
-};
+  /** Composition of the task */
+  task: PropTypes.shape({
+    /** Id of the task */
+    id: PropTypes.string.isRequired,
+    /** Title of the task */
+    title: PropTypes.string.isRequired,
+    /** Current state of the task */
+    state: PropTypes.string.isRequired,
+  }),
+  /** Event to change the task to archived */
+  onArchiveTask: PropTypes.func,
+  /** Event to change the task to pinned */
+  onPinTask: PropTypes.func,
+}
